d53b0661c420a76a0f94ee509ce9c88e8bcf2448
Jacob Meredith <software@jakemeredith.uk>
2025-09-06 00:51:47 +0100

    get window up and running

---
+++ Makefile
@@ -0,0 +1,24 @@
+TARGET = bin/final
+SRC = $(wildcard src/*.c)
+OBJ = $(patsubst src/%.c, obj/%.o, $(SRC))
+CFLAGS= -Iinclude -Ideps
+LIBS = -lglfw -lGL -lX11 -lpthread -lXrandr -lXi -ldl
+
+default: $(TARGET)
+
+clean:
+	rm -f obj/*.o
+	rm -f bin/*
+
+$(TARGET): $(OBJ)
+	gcc -o $@ $^ $(LIBS)
+
+obj/%.o : src/%.c
+	gcc -c $< -o $@ ${CFLAGS} 
+
+compile_flags.txt: force
+	@echo "Generating compile_flags.txt for IDE support"
+	@echo $(CFLAGS) | tr ' ' '\n' > $@
+
+force:
+
---
+++ src/main.c
@@ -0,0 +1,61 @@
+#include <glad.h>
+
+#include <GLFW/glfw3.h>
+#include <stdio.h>
+
+void framebuffer_size_callback(GLFWwindow *window, int width, int height) {
+  glViewport(0, 0, width, height);
+}
+
+void processInput(GLFWwindow *window) {
+  if (glfwGetKey(window, GLFW_KEY_ESCAPE) == GLFW_PRESS) {
+    glfwSetWindowShouldClose(window, 1);
+  }
+}
+
+int main(int argc, char *argv[]) {
+
+  glfwInit();
+  // Set opengl versions
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+  glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+
+  glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+
+  // Needed on macos
+  glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);
+
+  GLFWwindow *window = glfwCreateWindow(800, 600, "LearnOpenGL", NULL, NULL);
+  if (window == NULL) {
+    printf("Failed to create GLFW window\n");
+    glfwTerminate();
+    return -1;
+  }
+
+  glfwMakeContextCurrent(window);
+
+  // Init GLAD to load the address of the OpenGL function pointers (OS Specific)
+  if (!gladLoadGLLoader((GLADloadproc)glfwGetProcAddress)) {
+    printf("Failed to initialise GLAD\n");
+    return -1;
+  }
+
+  // Set rendering viewport and callback for resizing
+  glViewport(0, 0, 800, 600);
+  glfwSetFramebufferSizeCallback(window, framebuffer_size_callback);
+
+  while (!glfwWindowShouldClose(window)) {
+    processInput(window);
+
+    // This sets a value in the sate machine
+    glClearColor(0.2f, 0.3f, 0.3f, 1.0f);
+    // This uses the value
+    glClear(GL_COLOR_BUFFER_BIT);
+
+    glfwSwapBuffers(window);
+    glfwPollEvents();
+  }
+
+  glfwTerminate();
+  return 0;
+}
